c3c7f999996414f62a921945867dc62d3c01bd31
Guilherme Pires <guilherme.pires@ericsson.com>
2016-08-17 15:22:55 -0700

    Initial project. Added mongodb opsworks cookbook

---
+++ mongodb/metatada.rb
@@ -0,0 +1,8 @@
+name        "mongodb-opsworks"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "Guilherme.Pires@ericsson.com"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
+
